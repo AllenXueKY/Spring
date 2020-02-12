ea69f26263b5e9a2ea320565a7fd02b4a710c320
AllenXueKY <873661417@qq.com>
2020-02-12 20:25:57 +0800

    2020.02.12

--- day01_eesy_05DI/src/main/java/com/allen/service/impl/AccountServiceImpl2.java
+++ day01_eesy_05DI/src/main/java/com/allen/service/impl/AccountServiceImpl2.java
@@ -7,16 +7,22 @@ import java.util.Date;
 /**
  * 账户的业务层实现类
  */
-public class AccountServiceImpl implements IAccountService {
+public class AccountServiceImpl2 implements IAccountService {
 
     //如果是经常变化的数据，并不适用注入的方式
     private String a;
     private Integer b;
     private Date c;
 
-    public AccountServiceImpl(String a, Integer b, Date c) {
+    public void setA(String a) {
         this.a = a;
+    }
+
+    public void setB(Integer b) {
         this.b = b;
+    }
+
+    public void setC(Date c) {
         this.c = c;
     }
 
--- day01_eesy_05DI/src/main/java/com/allen/ui/Client.java
+++ day01_eesy_05DI/src/main/java/com/allen/ui/Client.java
@@ -17,10 +17,13 @@ public class Client {
         //1.获取核心容器对象
         ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         //2.根据id获取Bean对象
-        IAccountService as  = (IAccountService)ac.getBean("accountService");
-        as.saveAccount();
+//        IAccountService as  = (IAccountService)ac.getBean("accountService");
+//        as.saveAccount();
 
-        //手动关闭容器
-        ac.close();
+//        IAccountService as  = (IAccountService)ac.getBean("accountService2");
+//        as.saveAccount();
+
+        IAccountService as  = (IAccountService)ac.getBean("accountService3");
+        as.saveAccount();
     }
 }
